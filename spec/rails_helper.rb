4ddf5213dc976a83a6891542a9afee0d48ac18fe
mikamore <kim.roman-2002@yandex.ru>
2024-07-09 16:59:51 +0300

    Fix rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 require_relative 'support/factory_bot'
-require_relative 'support/chrome'
+# require_relative 'support/chrome'
+
+require 'shoulda/matchers'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
@@ -65,4 +67,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
